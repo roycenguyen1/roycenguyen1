83af08874029bf91ab69c159f98763e3854466af
Royce Nguyen <royce_nguyen@yahoo.com>
2020-10-01 23:56:59 -0400

    js file modified

--- js/main.js
+++ js/main.js
@@ -43,17 +43,15 @@ window.onscroll = function () {
   } else {
     header.style.background = "transparent";
   }
+  scrollFunction();
 };
 
 // Scroll to top button //
 
 //Get the button:
-mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function () {
-  scrollFunction();
-};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
