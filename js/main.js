4a1717650db7948e1ace4a9342b385685a7fbe69
Royce <68196495+roycenguyen1@users.noreply.github.com>
2020-10-04 03:22:13 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -49,7 +49,7 @@ $(function () {
 // Scroll to top button //
 
 //Get the button:
-mybutton = document.getElementById("myBtn");
+var mybutton = document.getElementById("myBtn");
 
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function () {
