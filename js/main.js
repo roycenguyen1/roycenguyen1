f7a6314bec59f31aad3ab6e0665f081347db7188
Vy Pham <vypham@Vys-MacBook-Pro.local>
2020-10-09 17:52:01 -0400

    add reload to logo

--- js/main.js
+++ js/main.js
@@ -74,7 +74,7 @@ $(function () {
 
 $(document).ready(function () {
   // Add smooth scrolling to all links
-  $("a").on("click", function (event) {
+  $('a[href*="#"]').on("click", function (event) {
     // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
       // Prevent default anchor click behavior
